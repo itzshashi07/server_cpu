34be431cf0162da5e153ffa1794230e9f076c405
itzshashi07 <itzshashi07@gmail.com>
2025-05-07 13:32:54 +0530

    Add all algorithm files

--- algorithms/roundrobin.js
+++ algorithms/roundrobin.js
@@ -57,6 +57,7 @@ module.exports = function roundRobin(processes, timeQuantum) {
       }
     }
   
+    
     return {
       processes: completed,
       averageWaitingTime: totalWaitingTime / completed.length,
